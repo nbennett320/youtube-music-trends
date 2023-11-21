7bc9cdfafdf26e5fd9c3386a40983db522d03144
Noah Bennett <nbennett.320@gmail.com>
2023-11-21 14:33:47 -0500

    playlist param targeting

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from playlist_fetch import PlaylistFetch
 def main():
   playlist_fetcher = PlaylistFetch()
 
-  playlist_fetcher.build_request_fields()
   playlist_fetcher.get_playlist()
 
 main()
--- playlist_fetch.py
+++ playlist_fetch.py
@@ -5,7 +5,6 @@ from spotify_client import Spotify
 class PlaylistFetch:
   playlist_id = '3CV4m7gpnCjTYPqcPoI2dc'
   playlist_url: str
-  spotify: Spotify
 
   field_structure = {
     'tracks': {
@@ -14,20 +13,47 @@ class PlaylistFetch:
       'items': {
         'track': {
           'name': 'name',
+          'artists': {
+            'name': 'name',
+          }
         }
       }
     }
   }
 
   _field = ''
+  _token: str
+  _spotify: Spotify
 
   def __init__(self):
     self.playlist_url = f"https://api.spotify.com/v1/playlists/{self.playlist_id}"
-    self.spotify = Spotify()
+    self._spotify = Spotify()
+    self._token = self._spotify.new_token()
+
+  def get_playlist(self):
+    access_token = self._token['access_token']
+
+    self._build_request_fields()
+
+    headers = {
+      'Authorization': f"Bearer {access_token}",
+    }
+
+    req = requests.get(
+      self.playlist_url,
+      headers=headers)
+
+    print(f"fields: {self._field}")
+    print(f"url: {self.playlist_url}")
+    # print(access_token)
+    # print(req)
+    print(req.json())
+
+  # PRIVATE METHODS
 
   # build request param from self.field_structure
   # ex: tracks(href,name,items(track(name)))
-  def build_request_fields(self):
+  def _build_request_fields(self):
     # format spotify field
     def traverse(root):
       items = root.items()
@@ -42,28 +68,15 @@ class PlaylistFetch:
           self._field = self._field + ')'
         else:
           self._field = self._field + ','
+        i = i + 1
         
     traverse(self.field_structure)
+
+    # trim trailing parenthesis
+    self._field = self._field[0:-1]
 
     # format and set url    
     field_param = urllib.parse.quote(self._field)
     print(field_param)
     self.playlist_url = f"{self.playlist_url}?fields={field_param}"
 
-  def get_playlist(self):
-    token = self.spotify.new_token()
-    access_token = token['access_token']
-
-    self.build_request_fields()
-
-    headers = {
-      'Authorization': f"Bearer {access_token}",
-    }
-
-    req = requests.get(
-      self.playlist_url,
-      headers=headers)
-
-    print(access_token)
-    print(req)
-    print(req.json())
