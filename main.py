3282a0708ccbfb4dfe1074d910144f440153a74a
Noah Bennett <nbennett.320@gmail.com>
2023-11-21 17:44:51 -0500

    fixing null filename bug, adding null uploader id bug fix to readme

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ def main():
   playlist_fetcher = PlaylistFetch()
   playlist_items = playlist_fetcher.get_playlist()
 
-  search_query = build_search_term(playlist_items['tracks']['items'][0])
+  search_query = build_search_term(playlist_items['tracks']['items'][10])
   yt_searcher = YoutubeSearch(search_query)
 
   print(f"search_query: {search_query}")
 
-  filename = yt_searcher.download_file()
+  yt_searcher.download_file()
+  filename = f"{yt_searcher.download_filename}.mp3"
 
   music_modifier = Music(filename)
   music_modifier.mod_random_tempo()
