a6ce72b67e310214e7d8385d1f450047c531b092
Noah Bennett <nbennett.320@gmail.com>
2023-11-21 20:47:42 -0500

    cleaning up commented code

--- main.py
+++ main.py
@@ -18,20 +18,20 @@ def build_search_term(item: dict[str, any]):
   return f"{artist} {title}"
 
 def main():
-  # playlist_fetcher = PlaylistFetch()
-  # playlist_items = playlist_fetcher.get_playlist()
+  playlist_fetcher = PlaylistFetch()
+  playlist_items = playlist_fetcher.get_playlist()
 
-  # search_query = build_search_term(playlist_items['tracks']['items'][10])
-  # yt_searcher = YoutubeSearch(search_query)
+  search_query = build_search_term(playlist_items['tracks']['items'][10])
+  yt_searcher = YoutubeSearch(search_query)
 
-  # print(f"search_query: {search_query}")
+  print(f"search_query: {search_query}")
 
-  # yt_searcher.download_file()
-  # filename = f"{yt_searcher.download_filename}.{yt_searcher.file_extension}"
+  yt_searcher.download_file()
+  filename = f"{yt_searcher.download_filename}.{yt_searcher.file_extension}"
 
-  # song_modifier = Song(filename)
-  # song_modifier.mod_random_tempo()
-  filename = 'tyla_water_faster.wav'
+  song_modifier = Song(filename)
+  song_modifier.mod_random_tempo()
+
   s = Song(filename)
   print(s.format_dur())
 
